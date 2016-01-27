eb567f28299579f559c269c9567cfb9800f1623a
kalasni <kalassni@gmail.com>
2016-01-27 21:28:46 +0100

    Create catalog listing y styling

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,9 +3,15 @@ class Product < ActiveRecord::Base
   validates :title, :description, :image_url, presence: true
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
   validates :title, uniqueness: true
+  validates_length_of :title, :minimum => 10, :message => "at least 10 characters"
   validates :image_url, allow_blank: true, format: {
     with: %r{\.(gif|jpg|png)\Z}i,
     message: 'must be a URL for GIF, JPG or PNG image.'
   }
+
+ # returns the most recently updated product
+  def self.latest
+    Product.order(:updated_at).last
+  end
 
 end
