6989ad9e5356f15c5bc907560425b3dffc1a16cc
kalasni <kalassni@gmail.com>
2016-01-28 19:57:43 +0100

    Cart creation

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  resources :line_items
+  resources :carts
   get 'store/index'
   resources :products
   # The priority is based upon order of creation: first created -> highest priority.
