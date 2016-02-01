2d2963ca11b100a94daed3274dca77032d88f5c9
kalasni <kalassni@gmail.com>
2016-02-01 20:47:54 +0100

    First part cart finished

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,7 +1,7 @@
 class LineItemsController < ApplicationController
 
   include CurrentCart # includes current_cart concern model
-  before_action :set_cart, only: [:create]
+  before_action :set_cart, only: [:create]  # current_cart method is to be involved before create() action
   before_action :set_line_item, only: [:show, :edit, :update, :destroy]
 
   # GET /line_items
@@ -28,19 +28,17 @@ class LineItemsController < ApplicationController
   # POST /line_items.json
   def create
     product = Product.find(params[:product_id])
-    @line_item = @cart.line_items.build(product: product)
+    @line_item = @cart.add_product(product.id, product.price)
 
-    respond_to do |format|
-      if @line_item.save
-        format.html { redirect_to @line_item.cart,
-          notice: 'Line item was successfully created.' }
-        format.json { render action: 'show',
-          status: :created, location: @line_item }
-      else
-        format.html { render action: 'new' }
-        format.json { render json: @line_item.errors,
-          status: :unprocessable_entity }
-      end
+    session[:counter] = 0
+      respond_to do |format|
+        if @line_item.save
+          format.html { redirect_to(@line_item.cart) }
+          format.xml  { render :xml => @line_item, :status => :created, :location => @line_item }
+        else
+          format.html { render :action => "new" }
+          format.xml  { render :xml => @line_item.errors, :status => :unprocessable_entity }
+        end
     end
   end
 
@@ -76,6 +74,6 @@ class LineItemsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def line_item_params
-      params.require(:line_item).permit(:product_id, :cart_id)
+      params.require(:line_item).permit(:product_id)
     end
 end
